7f962f43c65cfa319c5091a2941a49e1e265fd73
carallin <carallin_t@163.com>
2017-01-14 23:05:47 +0800

    make menu and article behave the same

--- newlab/public/js/commen.js
+++ newlab/public/js/commen.js
@@ -4,13 +4,35 @@ $(function(){
   var win = $(window);
   var doc = $(document);
   var headerHeight = $('#header').height();
+
+  var items = $('#article').find('.item');
+  var menu = $('#menu');
   win.scroll(function () {
-    if (doc.scrollTop() >= headerHeight) {
+    var top = doc.scrollTop();
+
+    //make menu and tip fixed
+    if (top >= headerHeight) {
       aside.addClass('fixed-aside');
       hintTop.removeClass('hidden');
     }else {
       aside.removeClass('fixed-aside');
       hintTop.addClass('hidden');
     };
+
+    //make menu and items in article 一致
+    var currentId = '';
+    items.each(function () {
+      if (top > $(this).offset().top - 50) { //根据实际情况决定在合适判定当前页面内容
+        currentId = '#' + $(this).attr('id');
+      }else {
+        return false;
+      }
+    });
+    var currentLink = menu.find('.current');
+    if (currentId && currentLink.attr('href') != currentId) {
+      currentLink.removeClass('current');
+      menu.find('[href="' + currentId + '"]').addClass('current');
+    }
+
   });
 });
