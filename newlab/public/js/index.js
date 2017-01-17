91a2ab914b43d155b1dd9929b0a1011bbdc4b1f4
carallin <carallin_t@163.com>
2017-01-18 04:54:42 +0800

    index.html change

---
+++ newlab/public/js/index.js
@@ -0,0 +1,81 @@
+$(function () {
+  var currentIndex = 0;
+  var images = $('.image-list .image-item');
+  var indexs = $('.index-list .index-item');
+  var len = images.length;
+
+  indexs.each(function (index) {
+    $(this).hover(function () {
+      //第一种方法：将changeTo直接写在程序中：
+      // if (currentIndex != index) {
+      //   $(indexs[currentIndex]).removeClass('current-index');
+      //   $(images[currentIndex]).fadeOut('fast');
+      //   $(images[index]).fadeIn('normal');
+      //   $(this).addClass('current-index');
+      //   currentIndex = index;
+      // }
+
+        changeTo(index,currentIndex);
+        currentIndex = index;
+    });
+  });
+
+  var autoChange = setInterval(function () {
+    var index = currentIndex;
+    if (currentIndex < len-1) {
+      currentIndex++;
+    }else {
+      currentIndex = 0;
+    }
+    changeTo(currentIndex, index);
+  },5000);
+
+//设置当滑入滑出显示区时的动作
+  $('#image-panel').hover(function () {
+    clearInterval(autoChange);
+    $('#direction').removeClass('hidden');
+  },function () {
+    $('#direction').addClass('hidden');
+    autoChange = setInterval(function () {
+      var index = currentIndex;
+      if (currentIndex < len-1) {
+        currentIndex++;
+      }else {
+        currentIndex = 0;
+      }
+      changeTo(currentIndex, index);
+    },5000);
+  });
+
+  $('#direction').on('click','li',function (e) {
+    if ($(e.target).attr('id') == $('#go-left').attr('id')) {
+      var index = currentIndex;
+      if (currentIndex > 0) {
+        currentIndex--;
+      }else {
+        currentIndex = len - 1;
+      }
+      changeTo(currentIndex, index);
+    }else if ($(e.target).attr('id') == $('#go-right').attr('id')) {
+      var index = currentIndex;
+      if (currentIndex < len-1) {
+        currentIndex++;
+      }else {
+        currentIndex = 0;
+      }
+      changeTo(currentIndex, index);
+    }
+  })
+
+});
+
+function changeTo(index,currentIndex) {
+  var images = $('.image-list .image-item');
+  var indexs = $('.index-list .index-item');
+  if (currentIndex != index) {
+    $(indexs[currentIndex]).removeClass('current-index');
+    $(images[currentIndex]).fadeOut('fast');
+    $(images[index]).fadeIn('slow');
+    $(indexs[index]).addClass('current-index');
+  }
+};
