814de2f4b878f49a86f466bb01176739f1f92d2d
Tian <I335793@global.corp.sap>
2017-03-10 17:52:24 +0800

    learning mongoDB--first time

--- node-demo/app.js
+++ node-demo/app.js
@@ -5,6 +5,38 @@ var bodyParser = require('body-parser');
 
 var router = require('./routes/index');
 
+// --------------------------------------mongoDB测试--------------------------------------------------
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error(tian):'));
+db.once('open', function (callback) {
+    var kittySchema = mongoose.Schema({
+        name: String
+    });
+    kittySchema.methods.speak = function () {
+        var greeting = this.name
+        ? "Meow name is " + this.name
+        : "I don't have a name!";
+        console.log(greeting);
+    }
+
+
+    var Kitten = mongoose.model('kitten', kittySchema);
+    var silence = new Kitten({name: 'Silence'});
+    console.log(silence.name);
+    var fluffy = new Kitten({ name: 'fluffy'});
+    fluffy.speak();
+
+    fluffy.save(function (err, fluffy) {
+        if(err) return console.error(err);
+        fluffy.speak();
+    });
+});
+
+// --------------------------------------mongoDB测试--------------------------------------------------
+
 app.set('views', __dirname + '/public/views');
 app.engine('.html', ejs.__express);
 app.set('view engine', 'html');
